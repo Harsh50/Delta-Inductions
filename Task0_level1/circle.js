c77651d9051bdff6de5b35b761005481d3d12d4b
Harsh50 <harveer19harsh01@gmail.com>
2014-05-30 14:25:49 +0530

    Add circle.js

---
+++ Task0_level1/circle.js
@@ -0,0 +1,23 @@
+
+window.onload = rotate;
+
+var ele=document.getElementById("ele");
+var increase=0.05;
+var angle=0;
+var x,y;
+x=parseInt(ele.style.left);
+y=parseInt(ele.style.top);
+
+function rotate()
+{
+ 
+
+x=150*Math.cos(angle)+250;
+y=150*Math.sin(angle)+250;
+
+ele.style.left=x+'px';
+ele.style.top=y+'px';
+angle+=increase;
+setTimeout(rotate,1);
+
+}
