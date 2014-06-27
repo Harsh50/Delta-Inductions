92286ed371bec07edaf4796dcacdb04285a05cb3
Harsh Kumar <harveer19harsh01@gmail.com>
2014-06-28 01:27:26 +0530

    Update validator.php

--- task2/validator.php
+++ task2/validator.php
@@ -33,9 +33,9 @@ function validateName()
 	$name=htmlspecialchars(trim($_POST["name"]));
 
 	
-	if(!preg_match("/^[A-Za-z ]*$/",$fname) and !empty($fname))
-	{ global $fname_error;
-	$fname_error ="* Invalid format! It should contain only alphabets";
+	if(!preg_match("/^[A-Za-z ]*$/",$name) and !empty($name))
+	{ global $name_error;
+	$name_error ="* Invalid format! It should contain only alphabets";
 	 return false;
 	}
 	
@@ -68,36 +68,15 @@ function validatePass()
 	return true;
 }
 
-function validateFile()
-{
-	if($_FILES['photo']['name'])
-	{
-		if(!$_FILES['photo']['error'])
-	   {
-	   	 $file_name=$_FILES['photo']['tmp_name'];
-	   	 if($_FILES['photo']['size'] > 200000) //can't be larger than 200 KB
-		  {
-			global $file_error;
-			$file_error="* File size should be less than 200KB!";
-			return false;
-		  }
-		  $currentdir = getcwd();
-        $target = $currentdir."/uploads/".$_POST["username"].strtolower($_FILES['photo']['name']);
-        
-		  move_uploaded_file($_FILES['photo']['tmp_name'], $target);
-		  return true;
-		  }
-		  
-		  else {
-		  	global $file_error;
-		  	$file_error=$_FILES['photo']['error'];
-		  	return false;
-		  	}
-	}
-	return true;
-}	  
+	  
 function validateRoll()
 {
+	 if(!preg_match("/^[0-9]{9}$/",$roll))
+ {
+ 	global $roll_error;
+ $roll_error="* It should be a 9 digit number";
+ return false;
+ }
 	$db=mysqli_connect("localhost","root","","students");
 	if(mysqli_connect_errno())
 	{
